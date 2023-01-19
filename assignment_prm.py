2f734ba66cf17122e6f01ce1c5bdfe27ff39f2ad
chaituchit36 <107355831+chaituchit36@users.noreply.github.com>
2023-01-19 21:22:43 +0530

    Create assignment_prm.py

---
+++ assignment_prm.py
@@ -0,0 +1,22 @@
+def triangle(i,j,k):
+    if i+j>=k and j+k>=i and k+i>=
+        return True
+    else:
+        return False
+trisides=list(map(int,input().spli
+
+if triangle(trisides[0], trisides[
+    print("Valid Triangle")
+else:
+    print("Invalid Triangle")
+      
+def rectangle(a,b,c,d):
+    if a==c and b==d:
+        return True
+    else:
+        return False
+rectsides=list(map(int,input().spl
+if rectangle(rectsides[0], rectsid
+    print("Valid Rectangle")
+else:
+    print("Invalid Rectangle")
